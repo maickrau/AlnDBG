0e93c5ed244a21e9c611001b684254a564dbb208
Mikko Rautiainen <m_rautiainen@hotmail.com>
2023-12-01 18:23:59 +0200

    remove unused code

--- src/GraphPhaser.h
+++ src/GraphPhaser.h
@@ -6,8 +6,6 @@
 #include "UnitigGraph.h"
 #include "MatchGroup.h"
 
-std::vector<std::pair<std::vector<size_t>, std::vector<size_t>>> getGraphPhaseBlockNodes(const UnitigGraph& unitigGraph, const std::vector<ReadPathBundle>& readPaths, const double approxOneHapCoverage);
-std::pair<UnitigGraph, std::vector<ReadPathBundle>> unzipGraphBubbles(const UnitigGraph& unitigGraph, const std::vector<ReadPathBundle>& readPaths, const std::vector<std::pair<std::vector<size_t>, std::vector<size_t>>>& phaseBlocks);
 std::pair<UnitigGraph, std::vector<ReadPathBundle>> unzipGraphLinearizable(const UnitigGraph& unitigGraph, const std::vector<ReadPathBundle>& readPaths, const double approxOneHapCoverage);
 
 #endif
