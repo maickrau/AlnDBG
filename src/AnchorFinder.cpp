d18f19db6ca2cdb6bd126b12a51e9c8670a532b2
Mikko Rautiainen <m_rautiainen@hotmail.com>
2024-01-17 17:08:24 +0200

    faster

--- src/AnchorFinder.cpp
+++ src/AnchorFinder.cpp
@@ -375,6 +375,33 @@ SparseEdgeContainer getAnchorEdges(const UnitigGraph& unitigGraph, const std::ve
 	return result;
 }
 
+std::vector<uint64_t> getReachableAnchors(const SparseEdgeContainer& edges, const uint64_t start, const std::vector<bool>& anchor)
+{
+	assert(!anchor[start & maskUint64_t]);
+	std::vector<uint64_t> stack;
+	stack.emplace_back(start);
+	phmap::flat_hash_set<uint64_t> visited;
+	while (stack.size() >= 1)
+	{
+		auto top = stack.back();
+		stack.pop_back();
+		if (visited.count(top) == 1) continue;
+		visited.insert(top);
+		if (anchor[top & maskUint64_t]) continue;
+		for (auto edge : edges.getEdges(std::make_pair(top & maskUint64_t, top & firstBitUint64_t)))
+		{
+			stack.push_back(edge.first + (edge.second ? firstBitUint64_t : 0));
+		}
+	}
+	std::vector<uint64_t> result;
+	for (uint64_t node : visited)
+	{
+		if (!anchor[node & maskUint64_t]) continue;
+		result.emplace_back(node);
+	}
+	return result;
+}
+
 void setReachableAnchors(const SparseEdgeContainer& edges, std::vector<std::vector<uint64_t>>& forwardReachableAnchors, std::vector<std::vector<uint64_t>>& backwardReachableAnchors, const uint64_t startNode, const std::vector<bool>& anchor)
 {
 	std::vector<uint64_t> stack;
@@ -446,25 +473,17 @@ void extendAnchors(const UnitigGraph& unitigGraph, const std::vector<ReadPathBun
 			tryExtendingThese[i] = false;
 		}
 	}
-	std::vector<std::vector<uint64_t>> forwardReachableAnchors;
-	std::vector<std::vector<uint64_t>> backwardReachableAnchors;
-	forwardReachableAnchors.resize(unitigGraph.nodeCount());
-	backwardReachableAnchors.resize(unitigGraph.nodeCount());
 	std::vector<bool> anchorsBeforeExtension = anchor;
-	for (size_t i = 0; i < unitigGraph.nodeCount(); i++)
-	{
-		if (!anchor[i]) continue;
-		if (!tryExtendingThese[i]) continue;
-		setReachableAnchors(edges, forwardReachableAnchors, backwardReachableAnchors, i + firstBitUint64_t, anchor);
-		setReachableAnchors(edges, forwardReachableAnchors, backwardReachableAnchors, i, anchor);
-	}
 	for (size_t i = 0; i < unitigGraph.nodeCount(); i++)
 	{
 		if (anchor[i]) continue;
 		if (!tryExtendingThese[i]) continue;
-		if (forwardReachableAnchors[i].size() != 1 && backwardReachableAnchors[i].size() != 1) continue;
-		if (forwardReachableAnchors[i].size() == 0 || backwardReachableAnchors[i].size() == 0) continue;
 		if (!cutsGraph(unitigGraph, edges, i, anchorsBeforeExtension)) continue;
+		auto fwAnchors = getReachableAnchors(edges, i+firstBitUint64_t, anchorsBeforeExtension);
+		if (fwAnchors.size() == 0) continue;
+		auto bwAnchors = getReachableAnchors(edges, i, anchorsBeforeExtension);
+		if (bwAnchors.size() == 0) continue;
+		if (fwAnchors.size() != 1 && bwAnchors.size() != 1) continue;
 		anchor[i] = true;
 	}
 }
@@ -509,19 +528,6 @@ std::vector<uint64_t> getChain(std::vector<bool>& checked, const size_t startNod
 {
 	assert(edges.size() == checked.size());
 	assert(startNode < checked.size());
-	for (size_t i = 0; i < edges.size(); i++)
-	{
-		std::pair<size_t, bool> fw { i, true };
-		for (auto pair : edges.getEdges(fw))
-		{
-			assert(edges.hasEdge(reverse(pair), reverse(fw)));
-		}
-		std::pair<size_t, bool> bw { i, false };
-		for (auto pair : edges.getEdges(bw))
-		{
-			assert(edges.hasEdge(reverse(pair), reverse(bw)));
-		}
-	}
 	assert(!checked[startNode]);
 	std::vector<uint64_t> fwChain = getChainOneWay(startNode + firstBitUint64_t, edges);
 	std::vector<uint64_t> bwChain = getChainOneWay(startNode, edges);
@@ -561,7 +567,7 @@ std::vector<uint64_t> getChain(std::vector<bool>& checked, const size_t startNod
 	return bwChain;
 }
 
-void getDistances(phmap::flat_hash_map<std::pair<uint64_t, uint64_t>, std::vector<int>>& distances, const std::vector<bool> anchor, const UnitigGraph& unitigGraph, const std::vector<ReadPathBundle>& readPaths)
+void getDistances(phmap::flat_hash_map<std::pair<uint64_t, uint64_t>, std::vector<int>>& distances, const std::vector<bool>& anchor, const UnitigGraph& unitigGraph, const std::vector<ReadPathBundle>& readPaths)
 {
 	for (size_t i = 0; i < readPaths.size(); i++)
 	{
@@ -641,7 +647,7 @@ double average(const std::vector<int>& values)
 	return sum / values.size();
 }
 
-void addOffsets(std::vector<AnchorChain>& chains, const std::vector<bool> anchor, const UnitigGraph& unitigGraph, const std::vector<ReadPathBundle>& readPaths)
+void addOffsets(std::vector<AnchorChain>& chains, const std::vector<bool>& anchor, const UnitigGraph& unitigGraph, const std::vector<ReadPathBundle>& readPaths)
 {
 	phmap::flat_hash_map<std::pair<uint64_t, uint64_t>, std::vector<int>> distances;
 	for (size_t i = 0; i < chains.size(); i++)
