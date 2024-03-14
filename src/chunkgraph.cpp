08c699ecb36399b8fc39e232d14359afa31e0de5
Mikko Rautiainen <m_rautiainen@hotmail.com>
2024-03-14 15:53:13 +0200

    cleaning

--- src/chunkgraph.cpp
+++ src/chunkgraph.cpp
@@ -35,6 +35,7 @@ class ChunkUnitigGraph
 {
 public:
 	std::vector<size_t> unitigLengths;
+	std::vector<std::vector<size_t>> chunksInUnitig;
 	std::vector<double> coverages;
 	SparseEdgeContainer edges;
 	phmap::flat_hash_map<std::pair<uint64_t, uint64_t>, size_t> edgeOverlaps;
@@ -2052,6 +2053,8 @@ std::tuple<ChunkUnitigGraph, std::vector<std::vector<UnitigPath>>> getChunkUniti
 	{
 		std::pair<size_t, bool> lastNode { unitigs[i].back() & maskUint64_t, unitigs[i].back() & firstBitUint64_t };
 		std::pair<size_t, bool> firstNode { unitigs[i][0] & maskUint64_t, (unitigs[i][0] ^ firstBitUint64_t) & firstBitUint64_t };
+		assert(!NonexistantChunk(unitigs[i].back()));
+		assert(!NonexistantChunk(unitigs[i][0]));
 		for (auto edge : allowedEdges.getEdges(lastNode))
 		{
 			uint64_t targetUnitig = std::get<0>(chunkLocationInUnitig[edge.first]);
@@ -2092,6 +2095,11 @@ std::tuple<ChunkUnitigGraph, std::vector<std::vector<UnitigPath>>> getChunkUniti
 		}
 		result.coverages[i] = sum / divisor;
 	}
+	result.chunksInUnitig.resize(result.unitigLengths.size());
+	for (size_t i = 0; i < unitigs.size(); i++)
+	{
+		result.chunksInUnitig[i] = unitigs[i];
+	}
 	auto paths = getUnitigPaths(result, chunksPerRead, allowedNode, unitigs, chunkLocationInUnitig);
 	return std::make_tuple(result, paths);
 }
@@ -2105,6 +2113,286 @@ void writeUnitigGraph(const std::string& graphFile, const std::string& pathsFile
 	writeUnitigGraph(graphFile, graph);
 	std::cerr << "writing unitig paths " << pathsFile << std::endl;
 	writeUnitigPaths(pathsFile, graph, readPaths, readNames, rawReadLengths);
+}
+
+std::vector<std::pair<uint64_t, std::vector<std::vector<size_t>>>> getUnitigForkReads(const ChunkUnitigGraph& graph, const std::vector<std::vector<UnitigPath>>& unitigPaths)
+{
+	std::vector<phmap::flat_hash_map<uint64_t, size_t>> forkAlleleToIndex;
+	phmap::flat_hash_map<uint64_t, size_t> forkToIndex;
+	std::vector<std::pair<uint64_t, std::vector<std::vector<size_t>>>> result;
+	for (size_t i = 0; i < graph.unitigLengths.size(); i++)
+	{
+		if (graph.edges.getEdges(std::make_pair(i, true)).size() >= 2)
+		{
+			size_t forkNumber = forkToIndex.size();
+			size_t alleleNumber = 0;
+			result.emplace_back();
+			result.back().first = i + firstBitUint64_t;
+			forkToIndex[i + firstBitUint64_t] = forkNumber;
+			forkAlleleToIndex.emplace_back();
+			assert(forkAlleleToIndex.size() == forkNumber+1);
+			for (auto edge : graph.edges.getEdges(std::make_pair(i, true)))
+			{
+				forkAlleleToIndex[forkNumber][edge.first + (edge.second ? firstBitUint64_t : 0)] = alleleNumber;
+				result.back().second.emplace_back();
+				alleleNumber += 1;
+			}
+		}
+		if (graph.edges.getEdges(std::make_pair(i, false)).size() >= 2)
+		{
+			size_t forkNumber = forkToIndex.size();
+			size_t alleleNumber = 0;
+			result.emplace_back();
+			result.back().first = i;
+			forkToIndex[i] = forkNumber;
+			forkAlleleToIndex.emplace_back();
+			assert(forkAlleleToIndex.size() == forkNumber+1);
+			for (auto edge : graph.edges.getEdges(std::make_pair(i, false)))
+			{
+				forkAlleleToIndex[forkNumber][edge.first + (edge.second ? firstBitUint64_t : 0)] = alleleNumber;
+				result.back().second.emplace_back();
+				alleleNumber += 1;
+			}
+		}
+	}
+	for (size_t i = 0; i < unitigPaths.size(); i++)
+	{
+		for (size_t j = 0; j < unitigPaths[i].size(); j++)
+		{
+			for (size_t k = 1; k < unitigPaths[i][j].path.size(); k++)
+			{
+				uint64_t prev = unitigPaths[i][j].path[k-1];
+				if (forkToIndex.count(prev) == 1)
+				{
+					size_t forkIndex = forkToIndex.at(prev);
+					uint64_t curr = unitigPaths[i][j].path[k];
+					assert(forkAlleleToIndex[forkIndex].count(curr) == 1);
+					result[forkIndex].second[forkAlleleToIndex[forkIndex].at(curr)].emplace_back(i);
+				}
+				uint64_t currRev = unitigPaths[i][j].path[k] ^ firstBitUint64_t;
+				if (forkToIndex.count(currRev) == 1)
+				{
+					size_t forkIndex = forkToIndex.at(currRev);
+					uint64_t prevRev = unitigPaths[i][j].path[k-1] ^ firstBitUint64_t;
+					assert(forkAlleleToIndex[forkIndex].count(prevRev) == 1);
+					result[forkIndex].second[forkAlleleToIndex[forkIndex].at(prevRev)].emplace_back(i);
+				}
+			}
+		}
+	}
+	for (size_t i = result.size()-1; i < result.size(); i--)
+	{
+		bool valid = true;
+		for (size_t j = 0; j < result[i].second.size(); j++)
+		{
+			std::sort(result[i].second[j].begin(), result[i].second[j].end());
+			for (size_t k = 1; k < result[i].second[j].size(); k++)
+			{
+				if (result[i].second[j][k-1] == result[i].second[j][k])
+				{
+					valid = false;
+					break;
+				}
+			}
+			if (!valid) break;
+		}
+		if (valid)
+		{
+			for (size_t j = 1; j < result[i].second.size(); j++)
+			{
+				for (size_t k = 0; k < j; k++)
+				{
+					if (intersectSize(result[i].second[j], result[i].second[k]) > 0)
+					{
+						valid = false;
+						break;
+					}
+				}
+				if (!valid) break;
+			}
+		}
+		if (!valid)
+		{
+			std::swap(result[i], result.back());
+			result.pop_back();
+		}
+	}
+	return result;
+}
+
+bool forkAllelesMatch(const std::vector<std::vector<size_t>>& left, const std::vector<std::vector<size_t>>& right, const size_t minMatch)
+{
+	assert(left.size() == right.size());
+	phmap::flat_hash_set<size_t> uniqueMatchesFound;
+	for (size_t i = 0; i < left.size(); i++)
+	{
+		size_t uniqueMatch = std::numeric_limits<size_t>::max();
+		for (size_t j = 0; j < right.size(); j++)
+		{
+			if (intersectSize(left[i], right[j]) == 0) continue;
+			if (intersectSize(left[i], right[j]) < minMatch) return false;
+			if (uniqueMatch != std::numeric_limits<size_t>::max()) return false;
+			uniqueMatch = j;
+		}
+		if (uniqueMatchesFound.count(uniqueMatch) == 1) return false;
+		uniqueMatchesFound.insert(uniqueMatch);
+	}
+	return true;
+}
+
+phmap::flat_hash_set<uint64_t> getSolidForks(const std::vector<std::pair<uint64_t, std::vector<std::vector<size_t>>>>& forkReads, const size_t minCoverage)
+{
+	std::vector<size_t> checkableIndices;
+	for (size_t i = 0; i < forkReads.size(); i++)
+	{
+		bool canCheckThis = true;
+		for (size_t j = 0; j < forkReads[i].second.size(); j++)
+		{
+			if (forkReads[i].second[j].size() < minCoverage)
+			{
+				canCheckThis = false;
+				break;
+			}
+		}
+		if (canCheckThis) checkableIndices.emplace_back(i);
+	}
+	std::vector<bool> solid;
+	solid.resize(checkableIndices.size(), false);
+	for (size_t i = 0; i < checkableIndices.size(); i++)
+	{
+		for (size_t j = 0; j < i; j++)
+		{
+			if (solid[i] && solid[j]) continue;
+			if (forkReads[checkableIndices[i]].second.size() != forkReads[checkableIndices[j]].second.size()) continue;
+			if (!forkAllelesMatch(forkReads[checkableIndices[i]].second, forkReads[checkableIndices[j]].second, minCoverage)) continue;
+			solid[i] = true;
+			solid[j] = true;
+		}
+	}
+	phmap::flat_hash_set<uint64_t> result;
+	for (size_t i = 0; i < checkableIndices.size(); i++)
+	{
+		if (!solid[i]) continue;
+		result.emplace(forkReads[checkableIndices[i]].first);
+	}
+	return result;
+}
+
+phmap::flat_hash_set<uint64_t> getAcceptableForks(const std::vector<std::pair<uint64_t, std::vector<std::vector<size_t>>>>& forkReads, const phmap::flat_hash_set<uint64_t>& solids, const size_t minCoverage)
+{
+	std::vector<size_t> solidIndices;
+	std::vector<size_t> checkableIndices;
+	for (size_t i = 0; i < forkReads.size(); i++)
+	{
+		if (solids.count(forkReads[i].first) == 1)
+		{
+			solidIndices.emplace_back(i);
+			continue;
+		}
+		bool canCheckThis = true;
+		for (size_t j = 0; j < forkReads[i].second.size(); j++)
+		{
+			if (forkReads[i].second[j].size() < minCoverage)
+			{
+				canCheckThis = false;
+				break;
+			}
+		}
+		if (canCheckThis) checkableIndices.emplace_back(i);
+	}
+	std::vector<bool> acceptable;
+	acceptable.resize(checkableIndices.size(), false);
+	for (size_t i = 0; i < checkableIndices.size(); i++)
+	{
+		for (size_t j : solidIndices)
+		{
+			if (forkReads[checkableIndices[i]].second.size() != forkReads[j].second.size()) continue;
+			if (!forkAllelesMatch(forkReads[checkableIndices[i]].second, forkReads[j].second, minCoverage)) continue;
+			acceptable[i] = true;
+			break;
+		}
+	}
+	phmap::flat_hash_set<uint64_t> result;
+	for (size_t i = 0; i < checkableIndices.size(); i++)
+	{
+		if (!acceptable[i]) continue;
+		result.emplace(forkReads[checkableIndices[i]].first);
+	}
+	return result;
+}
+
+bool hasOtherHigherCoverageEdge(const uint64_t fork, const uint64_t edge, const ChunkUnitigGraph& graph)
+{
+	std::pair<size_t, bool> forkpair { fork & maskUint64_t, fork & firstBitUint64_t };
+	std::pair<size_t, bool> edgepair { edge & maskUint64_t, edge & firstBitUint64_t };
+	assert(graph.edges.hasEdge(forkpair, edgepair));
+	auto keypair = MBG::canon(forkpair, edgepair);
+	std::pair<uint64_t, uint64_t> key { keypair.first.first + (keypair.first.second ? firstBitUint64_t : 0), keypair.second.first + (keypair.second.second ? firstBitUint64_t : 0) };
+	size_t compareCoverage = graph.edgeCoverages.at(key);
+	for (auto edge : graph.edges.getEdges(forkpair))
+	{
+		if (edge == edgepair) continue;
+		keypair = MBG::canon(forkpair, edge);
+		std::pair<uint64_t, uint64_t> key { keypair.first.first + (keypair.first.second ? firstBitUint64_t : 0), keypair.second.first + (keypair.second.second ? firstBitUint64_t : 0) };
+		if (graph.edgeCoverages.at(key) >= compareCoverage * 2) return true;
+	}
+	return false;
+}
+
+void cleanTips(const std::vector<TwobitString>& readSequences, std::vector<std::vector<std::tuple<size_t, size_t, uint64_t>>>& chunksPerRead, const size_t numThreads, const double approxOneHapCoverage)
+{
+	std::cerr << "cleaning tips" << std::endl;
+	ChunkUnitigGraph graph;
+	std::vector<std::vector<UnitigPath>> readPaths;
+	std::tie(graph, readPaths) = getChunkUnitigGraph(chunksPerRead);
+	phmap::flat_hash_set<uint64_t> solidFork;
+	phmap::flat_hash_set<uint64_t> acceptableFork;
+	{
+		std::vector<std::pair<uint64_t, std::vector<std::vector<size_t>>>> forkReads = getUnitigForkReads(graph, readPaths);
+		solidFork = getSolidForks(forkReads, approxOneHapCoverage * 0.5);
+		acceptableFork = getAcceptableForks(forkReads, solidFork, 3);
+	}
+	phmap::flat_hash_set<size_t> removeChunks;
+	size_t removeUnitigCount = 0;
+	size_t removeChunkCount = 0;
+	for (size_t i = 0; i < graph.unitigLengths.size(); i++)
+	{
+		if (graph.coverages[i] >= approxOneHapCoverage*0.5) continue;
+		if (graph.edges.getEdges(std::make_pair(i, true)).size() >= 2) continue;
+		if (graph.edges.getEdges(std::make_pair(i, false)).size() >= 2) continue;
+		if (graph.edges.getEdges(std::make_pair(i, true)).size() == 1)
+		{
+			std::pair<size_t, bool> otherpair = graph.edges.getEdges(std::make_pair(i, true))[0];
+			uint64_t other = otherpair.first + (otherpair.second ? firstBitUint64_t : 0);
+			other ^= firstBitUint64_t;
+			if (solidFork.count(other) == 1) continue;
+			if (acceptableFork.count(other) == 1) continue;
+			if (!hasOtherHigherCoverageEdge(other, i, graph)) continue;
+		}
+		if (graph.edges.getEdges(std::make_pair(i, false)).size() == 1)
+		{
+			std::pair<size_t, bool> otherpair = graph.edges.getEdges(std::make_pair(i, false))[0];
+			uint64_t other = otherpair.first + (otherpair.second ? firstBitUint64_t : 0);
+			other ^= firstBitUint64_t;
+			if (solidFork.count(other) == 1) continue;
+			if (acceptableFork.count(other) == 1) continue;
+			if (!hasOtherHigherCoverageEdge(other, i + firstBitUint64_t, graph)) continue;
+		}
+		removeUnitigCount += 1;
+		for (uint64_t chunk : graph.chunksInUnitig[i])
+		{
+			removeChunks.insert(chunk & maskUint64_t);
+		}
+	}
+	for (size_t i = 0; i < chunksPerRead.size(); i++)
+	{
+		for (size_t j = 0; j < chunksPerRead[i].size(); j++)
+		{
+			if (removeChunks.count(std::get<2>(chunksPerRead[i][j]) & maskUint64_t) == 0) continue;
+			std::get<2>(chunksPerRead[i][j]) = std::numeric_limits<size_t>::max();
+		}
+	}
+	std::cerr << "removed " << removeUnitigCount << " unitigs, " << removeChunks.size() << " chunks" << std::endl;
 }
 
 void makeGraph(const MatchIndex& matchIndex, const std::vector<std::string>& readNames, const std::vector<size_t>& rawReadLengths, const std::vector<TwobitString>& readSequences, const size_t numThreads)
@@ -2134,10 +2422,14 @@ void makeGraph(const MatchIndex& matchIndex, const std::vector<std::string>& rea
 //	countGoodKmersInChunks(readSequences, chunksPerRead, 1);
 //	countGoodishKmersInChunks(readSequences, chunksPerRead, 1);
 	writeUnitigGraph("graph-round9.gfa", "paths9.gaf", chunksPerRead, readNames, rawReadLengths);
-	removeSingleCopyChunks(chunksPerRead);
+	cleanTips(readSequences, chunksPerRead, numThreads, 20);
 	writeUnitigGraph("graph-round10.gfa", "paths10.gaf", chunksPerRead, readNames, rawReadLengths);
-	removeHighCoverageChunks(chunksPerRead, 60);
+	cleanTips(readSequences, chunksPerRead, numThreads, 20);
 	writeUnitigGraph("graph-round11.gfa", "paths11.gaf", chunksPerRead, readNames, rawReadLengths);
+	removeSingleCopyChunks(chunksPerRead);
+	writeUnitigGraph("graph-round12.gfa", "paths12.gaf", chunksPerRead, readNames, rawReadLengths);
+	removeHighCoverageChunks(chunksPerRead, 60);
+	writeUnitigGraph("graph-round13.gfa", "paths13.gaf", chunksPerRead, readNames, rawReadLengths);
 }
 
 int main(int argc, char** argv)
